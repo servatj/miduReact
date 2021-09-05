9591a559bbf3dcb254b33fdad881ecde93366a20
Josep Servat <jservat@outlook.es>
2021-09-05 12:37:02 +0200

    use npm

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -3,10 +3,10 @@ import './Gif.css';
 
 export default function gif({ id, url, title }) {
   return (
-    <div className="Gif">
+    <a href={url} className="Gif">
       <h4>{title}</h4>
       <h5>{id}</h5>
       <img src={url} alt={`gif${title}`} />
-    </div>
+    </a>
   );
 }
