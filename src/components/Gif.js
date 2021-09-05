05719fdfb514bdd38d514b012389045f829c339f
Josep Servat <jservat@outlook.es>
2021-09-05 12:32:12 +0200

    Add a bit more style

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './Gif.css';
 
 export default function gif({ id, url, title }) {
   return (
-    <>
+    <div className="Gif">
       <h4>{title}</h4>
       <h5>{id}</h5>
       <img src={url} alt={`gif${title}`} />
-    </>
+    </div>
   );
 }
