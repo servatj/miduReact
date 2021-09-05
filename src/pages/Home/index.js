68971a31172809badfca848470c414434734ea8d
Josep Servat <jservat@outlook.es>
2021-09-05 17:29:10 +0200

    Add Search and third party hook

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,26 @@
-import React from "react";
-import { Link } from "wouter";
+import React, { useState } from "react";
+import { Link, useLocation } from "wouter";
 
 const FOOTBALL_GIFS = ["Messi", "Neymar", "CR7", "Mbappe"];
 
 export default function Home() {
+  const [keyword, setKeyword] = useState('');
+  const [, setLocation] = useLocation();
+
+  const handlerSubmit = (evt) => {
+    evt.preventDefault();
+    setLocation(`/search/${keyword}`)
+  }
+
+  const handelChange = (evt) => {
+    setKeyword(evt.target.value);
+  }
+
   return (
     <>
+      <form onSubmit={handlerSubmit}>
+        <input type="text" value={keyword} placeholder=" | search" onChange={handelChange}></input>
+      </form>
       <title>Home | Giffy</title>
       <h3 className="App-title">Most Popular Gifs</h3>
       <ul>
